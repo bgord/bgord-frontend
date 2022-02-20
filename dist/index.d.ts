b0a7d8e29ecf4d7cd19532dcbbe454a21ed267ee
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-02-20 20:44:06 +0100

    feature: add use expandable list hook

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -12,3 +12,20 @@ export declare type WindowDimensions = {
 export declare function useWindowDimensions(): WindowDimensions;
 export declare function useScrollLock(condition?: boolean): void;
 export declare function usePreviousValue<T>(value: T): T | null;
+export declare type UseExpandableListConfigType = {
+    max: number;
+    length: number;
+};
+export declare enum UseExpandableListState {
+    contracted = "contracted",
+    expanded = "expanded"
+}
+export declare function useExpandableList(config: UseExpandableListConfigType): {
+    state: UseExpandableListState;
+    displayShowMore: boolean;
+    displayShowLess: boolean;
+    showMore: () => void;
+    showLess: () => void;
+    numberOfExcessiveElements: number;
+    filterFn: (_element: any, index: number) => boolean;
+};
