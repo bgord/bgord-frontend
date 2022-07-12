c24acdf026847ec54a2e2b0e8e2f00099aeeba32
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-07-12 19:52:43 +0200

    step: make date formatter method static

--- dates.ts
+++ dates.ts
@@ -1,7 +1,7 @@
 export type DateType = Date | number | null | undefined;
 
 export class DateFormatter {
-  datetime(date: DateType, defaultValue = "N/A"): string {
+  static datetime(date: DateType, defaultValue = "N/A"): string {
     if (!date) return defaultValue;
 
     return new Date(date).toLocaleString();
--- dist/dates.d.ts
+++ dist/dates.d.ts
@@ -1,4 +1,4 @@
 export declare type DateType = Date | number | null | undefined;
 export declare class DateFormatter {
-    datetime(date: DateType, defaultValue?: string): string;
+    static datetime(date: DateType, defaultValue?: string): string;
 }
