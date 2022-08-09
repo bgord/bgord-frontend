d6a1e2291cafcc445258675be63979c81e453cca
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-08-09 16:50:38 +0200

    refactor: add dialog component

---
+++ dist/components/dialog.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+import * as hooks from "../hooks";
+export declare type DialogPropsType = hooks.UseToggleReturnType & JSX.IntrinsicElements["dialog"] & {
+    DialogOverlay?: JSX.Element;
+};
+export declare function Dialog(props: DialogPropsType): JSX.Element | null;
+export declare function DefaultDialogOverlay(props: JSX.IntrinsicElements["div"]): JSX.Element;
