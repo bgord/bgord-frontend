cde7661b5a9dff6abfb23f8db10dcaee963b9941
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-08-11 21:31:40 +0200

    feature: add use document title hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,6 +2,7 @@ export * from "./use-autofocus";
 export * from "./use-breakpoint";
 export * from "./use-click-outside";
 export * from "./use-client-search";
+export * from "./use-document-title";
 export * from "./use-expandable-list";
 export * from "./use-field";
 export * from "./use-file";
