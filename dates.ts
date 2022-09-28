858070308aacf9f5ac65f4beff1130f980a57812
Bartosz Gordon <gordonbartosz@gmail.com>
2022-09-29 00:26:57 +0200

    feature: add DateFormatter#form

--- dates.ts
+++ dates.ts
@@ -6,4 +6,14 @@ export class DateFormatter {
 
     return new Date(date).toLocaleString();
   }
+
+  static form(date: Date | null): string {
+    if (!date) return DateFormatter.form(new Date());
+
+    const year = String(date.getFullYear()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  }
 }
--- dist/dates.d.ts
+++ dist/dates.d.ts
@@ -1,4 +1,5 @@
 export declare type DateType = Date | number | null | undefined;
 export declare class DateFormatter {
     static datetime(date: DateType, defaultValue?: string): string;
+    static form(date: Date | null): string;
 }
