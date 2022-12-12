0406a522e5e9b756a7f250b4c03bce8efb0e6c98
Bartosz Gordon <gordonbartosz@gmail.com>
2022-12-13 00:38:06 +0100

    feature: add static lower time attributes

--- dist/time.d.ts
+++ dist/time.d.ts
@@ -1,5 +1,9 @@
 export declare class Days {
     value: number;
+    static hours: number;
+    static minutes: number;
+    static seconds: number;
+    static ms: number;
     constructor(value: number);
     toHours(): number;
     toMinutes(): number;
@@ -8,6 +12,9 @@ export declare class Days {
 }
 export declare class Hours {
     value: number;
+    static minutes: number;
+    static seconds: number;
+    static ms: number;
     constructor(value: number);
     toMinutes(): number;
     toSeconds(): number;
@@ -15,12 +22,15 @@ export declare class Hours {
 }
 export declare class Minutes {
     value: number;
+    static seconds: number;
+    static ms: number;
     constructor(value: number);
     toSeconds(): number;
     toMs(): number;
 }
 export declare class Seconds {
     value: number;
+    static ms: number;
     constructor(value: number);
     toMs(): number;
 }
--- time.ts
+++ time.ts
@@ -1,72 +1,92 @@
 export class Days {
   value: number;
 
+  static hours = 24;
+
+  static minutes = 24 * 60;
+
+  static seconds = 24 * 60 * 60;
+
+  static ms = 24 * 60 * 60 * 1000;
+
   constructor(value: number) {
     this.value = value;
   }
 
   toHours() {
-    return this.value * 24;
+    return this.value * Days.hours;
   }
 
   toMinutes() {
-    return this.value * 24 * 60;
+    return this.value * Days.minutes;
   }
 
   toSeconds() {
-    return this.value * 24 * 60 * 60;
+    return this.value * Days.seconds;
   }
 
   toMs() {
-    return this.value * 24 * 60 * 60 * 1000;
+    return this.value * Days.ms;
   }
 }
 
 export class Hours {
   value: number;
 
+  static minutes = 60;
+
+  static seconds = 60 * 60;
+
+  static ms = 60 * 60 * 1000;
+
   constructor(value: number) {
     this.value = value;
   }
 
   toMinutes() {
-    return this.value * 60;
+    return this.value * Hours.minutes;
   }
 
   toSeconds() {
-    return this.value * 60 * 60;
+    return this.value * Hours.seconds;
   }
 
   toMs() {
-    return this.value * 60 * 60 * 1000;
+    return this.value * Hours.ms;
   }
 }
 
 export class Minutes {
   value: number;
 
+  static seconds = 60 * 1000;
+
+  static ms = 1000;
+
   constructor(value: number) {
     this.value = value;
   }
 
   toSeconds() {
-    return this.value * 60;
+    return this.value * Minutes.seconds;
   }
 
   toMs() {
-    return this.value * 60 * 1000;
+    return this.value * Minutes.ms;
   }
 }
 
 export class Seconds {
   value: number;
 
+  static ms = 1000;
+
   constructor(value: number) {
     this.value = value;
   }
 
   toMs() {
-    return this.value * 1000;
+    return this.value * Seconds.ms;
   }
 }
 
