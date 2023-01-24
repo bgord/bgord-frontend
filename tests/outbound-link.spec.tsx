c0e2f1bb1359a6e0f46ae2c3b680bad45b72ef1c
Bartosz Gordon <gordonbartosz@gmail.com>
2023-01-24 22:43:33 +0100

    step: test outbound-link

--- tests/outbound-link.spec.tsx
+++ tests/outbound-link.spec.tsx
@@ -4,10 +4,44 @@ import { render, screen } from "@testing-library/react";
 
 import { OutboundLink } from "../components/outbound-link";
 
-describe("outbound-link", () => {
-  it("false to be false", () => {
-    render(<OutboundLink href="https://google.com" />);
+const link = "https://google.com";
+const text = "google link";
 
-    screen.debug();
+describe("outbound-link", () => {
+  it("renders link with a text", () => {
+    render(<OutboundLink href={link}>{text}</OutboundLink>);
+
+    expect(screen.getByText(text)).toHaveAttribute("href", link);
+    expect(screen.getByText(text)).toHaveAttribute("rel", "noreferer noopener");
+    expect(screen.getByText(text)).toHaveAttribute("target", "_blank");
+  });
+
+  it("passes down data-* attributes", () => {
+    render(
+      <OutboundLink href={link} data-display="flex">
+        {text}
+      </OutboundLink>
+    );
+
+    expect(screen.getByText(text)).toHaveAttribute("data-display", "flex");
+  });
+
+  it("handles as prop correctly", () => {
+    type CustomLinkProps = { href: string } & Record<string, unknown>;
+
+    function CustomLink({ href, ...props }: CustomLinkProps) {
+      return (
+        <div>
+          <a href={href} {...props}>
+            {text}
+          </a>
+        </div>
+      );
+    }
+
+    render(<OutboundLink as={CustomLink} href={link} data-display="flex" />);
+
+    expect(screen.getByText(text)).toHaveAttribute("rel", "noreferer noopener");
+    expect(screen.getByText(text)).toHaveAttribute("target", "_blank");
   });
 });
