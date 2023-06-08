b103debc1761e9ec193066877526b8d9cdd6e650
Bartosz Gordon <gordonbartosz@gmail.com>
2023-06-08 20:34:53 +0200

    step: return passed value in time.* methods

--- dist/time.d.ts
+++ dist/time.d.ts
@@ -1,19 +1,23 @@
 export declare function Days(value: number): {
+    value: number;
     toHours(): number;
     toMinutes(): number;
     toSeconds(): number;
     toMs(): number;
 };
 export declare function Hours(value: number): {
+    value: number;
     toMinutes(): number;
     toSeconds(): number;
     toMs(): number;
 };
 export declare function Minutes(value: number): {
+    value: number;
     toSeconds(): number;
     toMs(): number;
 };
 export declare function Seconds(value: number): {
+    value: number;
     toMs(): number;
 };
 export declare const Time: {
