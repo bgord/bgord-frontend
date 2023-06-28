9f9f31a4c80f18a8caa30ae780dc11f1e6120b4c
Bartosz Gordon <gordonbartosz@gmail.com>
2023-06-28 21:39:44 +0200

    step: add SafeLocalStorage#clear

--- safe-local-storage.ts
+++ safe-local-storage.ts
@@ -1,11 +1,23 @@
 import * as Storage from "ts-storage";
 
+export type SafeLocalStorageKeyType = string;
+
 export class SafeLocalStorage {
-  static get<T extends Storage.AllowedTypes>(key: string, fallbackValue: T): T {
+  static get<T extends Storage.AllowedTypes>(
+    key: SafeLocalStorageKeyType,
+    fallbackValue: T
+  ): T {
     return Storage.get(key, fallbackValue).value;
   }
 
-  static set<T extends Storage.AllowedTypes>(key: string, value: T) {
+  static set<T extends Storage.AllowedTypes>(
+    key: SafeLocalStorageKeyType,
+    value: T
+  ) {
     Storage.set(key, value);
+  }
+
+  static clear(key: SafeLocalStorageKeyType) {
+    Storage.remove(key);
   }
 }
