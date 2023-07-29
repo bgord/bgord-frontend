1a2f33d9aa541f7fe4060f9c4d4d1fb6c9cffed8
Bartosz Gordon <gordonbartosz@gmail.com>
2023-07-29 17:40:31 +0200

    feature: add accept-language header to api

--- api.ts
+++ api.ts
@@ -1,4 +1,5 @@
 import { ServerError } from "./server-error";
+import { SafeLocalStorage } from "./safe-local-storage";
 
 export const API: typeof fetch = (input, init) =>
   fetch(input, {
@@ -7,6 +8,7 @@ export const API: typeof fetch = (input, init) =>
       "Content-Type": "application/json",
 
       "time-zone-offset": new Date().getTimezoneOffset().toString(),
+      "accept-language": SafeLocalStorage.get("accept-language", "en"),
     },
     redirect: "follow",
     ...init,
