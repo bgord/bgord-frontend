da6285a7c277bcd107c19978250128ee68b7a589
Bartosz Gordon <gordonbartosz@gmail.com>
2023-07-29 19:38:27 +0200

    step: remove accept language header from api

--- api.ts
+++ api.ts
@@ -1,5 +1,4 @@
 import { ServerError } from "./server-error";
-import { SafeLocalStorage } from "./safe-local-storage";
 
 export const API: typeof fetch = (input, init) =>
   fetch(input, {
@@ -8,7 +7,6 @@ export const API: typeof fetch = (input, init) =>
       "Content-Type": "application/json",
 
       "time-zone-offset": new Date().getTimezoneOffset().toString(),
-      "accept-language": SafeLocalStorage.get("accept-language", "en"),
     },
     redirect: "follow",
     ...init,
