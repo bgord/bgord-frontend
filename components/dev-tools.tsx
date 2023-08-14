c6de8a2c9fff08917527f56d2ee1a300cbbad31f
Bartosz Gordon <gordonbartosz@gmail.com>
2023-08-14 11:52:15 +0200

    feature: add dev tools truncates

--- components/dev-tools.tsx
+++ components/dev-tools.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-import { useWindowDimensions } from "../hooks";
+import { useWindowDimensions } from "../hooks/use-window-dimensions";
+import { useToggle } from "../hooks/use-toggle";
+import { useField } from "../hooks/use-field";
+import { exec } from "../exec";
 
 function Dimensions(props: JSX.IntrinsicElements["div"]) {
   const dimensions = useWindowDimensions();
@@ -12,4 +15,55 @@ function Dimensions(props: JSX.IntrinsicElements["div"]) {
   );
 }
 
-export const DevTools = { Dimensions };
+function Truncates() {
+  const enabled = useToggle();
+  const length = useField("length", 128);
+
+  const [cache, setCache] = React.useState(new Map());
+
+  const handleTruncateClick = () => {
+    const elements = document.querySelectorAll('[data-transform="truncate"]');
+
+    const updatedCache = new Map(cache);
+
+    elements.forEach((element) => {
+      const originalText = element.textContent;
+
+      if (!updatedCache.has(element)) {
+        updatedCache.set(element, originalText);
+        element.textContent = "x".repeat(length.value);
+      } else {
+        element.textContent = updatedCache.get(element);
+        updatedCache.delete(element);
+      }
+    });
+
+    setCache(updatedCache);
+  };
+
+  return (
+    <div data-display="flex" data-cross="center" data-gap="6">
+      <label className="c-label" {...length.label.props}>
+        Length
+      </label>
+
+      <input
+        className="c-input"
+        type="number"
+        value={length.value}
+        onChange={(event) => length.set(event.currentTarget.valueAsNumber)}
+        {...length.input.props}
+      />
+      <button
+        className="c-button"
+        data-variant="bare"
+        type="button"
+        onClick={exec([() => handleTruncateClick(), enabled.toggle])}
+      >
+        {enabled.on ? "Hide truncates" : "Expand truncates"}
+      </button>
+    </div>
+  );
+}
+
+export const DevTools = { Dimensions, Truncates };
--- dist/components/dev-tools.d.ts
+++ dist/components/dev-tools.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="react" />
 declare function Dimensions(props: JSX.IntrinsicElements["div"]): JSX.Element;
+declare function Truncates(): JSX.Element;
 export declare const DevTools: {
     Dimensions: typeof Dimensions;
+    Truncates: typeof Truncates;
 };
 export {};
