20a6915441e4f80f554d214bb6bedc3ed6a88786
Bartosz Gordon <gordonbartosz@gmail.com>
2023-08-23 18:48:00 +0200

    fix: add use toggle config type

--- hooks/use-toggle.ts
+++ hooks/use-toggle.ts
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export type UseToggleValueType = boolean;
 
+export type UseToggleConfigType = UseToggleValueType;
+
 export type UseToggleReturnType = {
   on: UseToggleValueType;
   off: UseToggleValueType;
@@ -10,7 +12,9 @@ export type UseToggleReturnType = {
   toggle: VoidFunction;
 };
 
-export function useToggle(defaultValue = false): UseToggleReturnType {
+export function useToggle(
+  defaultValue: UseToggleConfigType = false
+): UseToggleReturnType {
   const [on, setIsOn] = useState(defaultValue);
 
   const enable = () => setIsOn(true);
