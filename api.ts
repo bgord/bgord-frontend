5af07f68aae8d3450e6515578636b76e0e46331d
Bartosz Gordon <gordonbartosz@gmail.com>
2023-11-18 14:46:34 +0100

    fix: api headersl

--- api.ts
+++ api.ts
@@ -3,14 +3,14 @@ import { ServerError } from "./server-error";
 export const API: typeof fetch = (input, init) =>
   fetch(input, {
     mode: "same-origin",
+    redirect: "follow",
+    ...init,
     headers: {
       "Content-Type": "application/json",
 
       "time-zone-offset": new Date().getTimezoneOffset().toString(),
       ...init?.headers,
     },
-    redirect: "follow",
-    ...init,
   })
     .then(ServerError.extract)
     .catch(ServerError.handle);
