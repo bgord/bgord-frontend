a4c13723e079c18948d72b6b3a55d1b70397d045
Bartosz Gordon <gordonbartosz@gmail.com>
2024-01-22 15:19:44 +0100

    feature: add enabled option to use keyboard shortcuts hook

--- dist/hooks/use-keyboard-shortcuts.d.ts
+++ dist/hooks/use-keyboard-shortcuts.d.ts
@@ -1,4 +1,7 @@
 export interface UseKeyboardShortcutsConfigType {
     [keybinding: string]: (event: KeyboardEvent) => void;
 }
-export declare function useKeyboardShortcuts(config: UseKeyboardShortcutsConfigType): void;
+export type UseKeyboardShortcutsOptionsType = {
+    enabled?: boolean;
+};
+export declare function useKeyboardShortcuts(config: UseKeyboardShortcutsConfigType, options?: UseKeyboardShortcutsOptionsType): void;
--- hooks/use-keyboard-shortcuts.ts
+++ hooks/use-keyboard-shortcuts.ts
@@ -5,12 +5,19 @@ export interface UseKeyboardShortcutsConfigType {
   [keybinding: string]: (event: KeyboardEvent) => void;
 }
 
+export type UseKeyboardShortcutsOptionsType = { enabled?: boolean };
+
 export function useKeyboardShortcuts(
-  config: UseKeyboardShortcutsConfigType
+  config: UseKeyboardShortcutsConfigType,
+  options?: UseKeyboardShortcutsOptionsType
 ): void {
+  const enabled = options?.enabled ?? true;
+
   React.useEffect(() => {
+    if (!enabled) return;
+
     const unsubscribeShortcuts = tinykeys(window, config);
 
     return () => unsubscribeShortcuts();
-  }, [config]);
+  }, [config, enabled]);
 }
