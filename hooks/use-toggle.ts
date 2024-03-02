74d33fd46580c1ec7f2f7580b5b0bdc262c8df5e
Bartosz Gordon <gordonbartosz@gmail.com>
2024-03-02 18:59:16 +0100

    feature: add aria-expanded related props to use toggle

--- hooks/use-toggle.ts
+++ hooks/use-toggle.ts
@@ -10,10 +10,18 @@ export type UseToggleReturnType = {
   enable: VoidFunction;
   disable: VoidFunction;
   toggle: VoidFunction;
+  props: {
+    controller: {
+      "aria-expanded": string;
+      "aria-controls": string | undefined;
+    };
+    target: { id: string | undefined };
+  };
 };
 
 export function useToggle(
-  defaultValue: UseToggleConfigType = false
+  defaultValue: UseToggleConfigType = false,
+  name?: string
 ): UseToggleReturnType {
   const [on, setIsOn] = useState(defaultValue);
 
@@ -21,14 +29,27 @@ export function useToggle(
   const disable = () => setIsOn(false);
   const toggle = () => setIsOn((v) => !v);
 
-  return { on, off: !on, enable, disable, toggle };
+  return {
+    on,
+    off: !on,
+    enable,
+    disable,
+    toggle,
+    props: {
+      controller: { "aria-expanded": String(on), "aria-controls": name },
+      target: { id: name },
+    },
+  };
 }
 
-export function extractUseToggle<X>(props: UseToggleReturnType & X): {
+export function extractUseToggle<X>(_props: UseToggleReturnType & X): {
   toggle: UseToggleReturnType;
   rest: X;
 } {
-  const { on, off, enable, disable, toggle, ...rest } = props;
+  const { on, off, enable, disable, toggle, props, ...rest } = _props;
 
-  return { toggle: { on, off, enable, disable, toggle }, rest: rest as X };
+  return {
+    toggle: { on, off, enable, disable, toggle, props },
+    rest: rest as X,
+  };
 }
