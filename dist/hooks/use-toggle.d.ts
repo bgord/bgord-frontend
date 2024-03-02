3430e8c9e54f04418342464a7e23ea10a02ea884
Bartosz Gordon <gordonbartosz@gmail.com>
2024-03-02 18:59:16 +0100

    feature: add aria-expanded related props to use toggle

--- dist/hooks/use-toggle.d.ts
+++ dist/hooks/use-toggle.d.ts
@@ -6,9 +6,18 @@ export type UseToggleReturnType = {
     enable: VoidFunction;
     disable: VoidFunction;
     toggle: VoidFunction;
+    props: {
+        controller: {
+            "aria-expanded": string;
+            "aria-controls": string | undefined;
+        };
+        target: {
+            id: string | undefined;
+        };
+    };
 };
-export declare function useToggle(defaultValue?: UseToggleConfigType): UseToggleReturnType;
-export declare function extractUseToggle<X>(props: UseToggleReturnType & X): {
+export declare function useToggle(defaultValue?: UseToggleConfigType, name?: string): UseToggleReturnType;
+export declare function extractUseToggle<X>(_props: UseToggleReturnType & X): {
     toggle: UseToggleReturnType;
     rest: X;
 };
