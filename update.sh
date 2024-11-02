40534f3a2051338b84bae3b3f0ce7ebb3ba40eb5
Bartosz Gordon <gordonbartosz@gmail.com>
2024-11-02 11:38:05 +0100

    fix: update script

--- update.sh
+++ update.sh
@@ -5,11 +5,11 @@ setup_base_config
 
 info "Performing an auto-update..."
 
-cd bgord-frontend/
+cd frontend/bgord-frontend/
 git pull origin master
 
-cd ../
-git add bgord-frontend/
+cd ../../
+git add frontend/bgord-frontend
 git commit -m "step: bump bgord/frontend" --no-verify
 
 success "Auto-update performed!"
