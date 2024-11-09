639ba05fa6a2e8329a7d5e1e8c633fa66505a1f8
Bartosz Gordon <gordonbartosz@gmail.com>
2024-11-09 21:07:23 +0100

    step: implement params field tests

--- tests/params-field.test.ts
+++ tests/params-field.test.ts
@@ -1,13 +1,38 @@
-import { describe, test } from "vitest";
+import { expect, describe, test } from "vitest";
+
+import { ParamsField } from "../hooks/params-field";
 
 describe("ParamsField", () => {
-  test.todo("create - empty value");
-  test.todo("create - non-empty value");
-  test.todo("is empty - null");
-  test.todo("is empty - undefined");
-  test.todo("is empty - empty string");
-  test.todo("get");
-  test.todo("compare - empty and empty");
-  test.todo("compare - empty and non-empty");
-  test.todo("compare - non-empty and non-empty");
+  test("create - empty value", () => {
+    const result = new ParamsField(ParamsField.emptyValue);
+    expect(result.get()).toEqual(ParamsField.emptyValue);
+    expect(result.isEmpty()).toEqual(true);
+  });
+  test("create - empty string", () => {
+    const result = new ParamsField("");
+    expect(result.get()).toEqual(ParamsField.emptyValue);
+    expect(result.isEmpty()).toEqual(true);
+  });
+  test("create - null", () => {
+    const result = new ParamsField(null);
+    expect(result.get()).toEqual(ParamsField.emptyValue);
+    expect(result.isEmpty()).toEqual(true);
+  });
+  test("create - non-empty value", () => {
+    const result = new ParamsField("abc");
+    expect(result.get()).toEqual("abc");
+    expect(result.isEmpty()).toEqual(false);
+  });
+  test("compare - empty and empty", () => {
+    expect(ParamsField.compare(undefined, undefined)).toEqual(true);
+  });
+  test("compare - empty and non-empty", () => {
+    expect(ParamsField.compare(undefined, "abc")).toEqual(false);
+  });
+  test("compare - non-empty and non-empty - success", () => {
+    expect(ParamsField.compare("abc", "abc")).toEqual(true);
+  });
+  test("compare - non-empty and non-empty - failure", () => {
+    expect(ParamsField.compare("def", "abc")).toEqual(false);
+  });
 });
