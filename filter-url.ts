de973954ae02298e9bd0821481d0d2e3bb8e0f24
Bartosz Gordon <gordonbartosz@gmail.com>
2024-11-11 14:29:38 +0100

    refactor: improve filter url

--- filter-url.ts
+++ filter-url.ts
@@ -1,28 +1,25 @@
+import { Field } from "./hooks/field";
+
 export type FilterType = Record<string, unknown> | undefined;
 
 export class FilterUrl {
   value: string;
 
   constructor(url: string, filters?: FilterType) {
-    const nonEmptyFilters = this.getNonEmptyFilters(filters);
-    const query = new URLSearchParams(nonEmptyFilters);
+    this.value = url;
 
-    if (query.toString() === "") {
-      this.value = url;
+    if (!filters) return;
 
-      return;
-    }
+    const params = Object.entries(filters).filter(
+      ([, value]) => !Field.isEmpty(value as any),
+    );
+
+    if (!params.length) return;
+
+    const query = new URLSearchParams(
+      Object.fromEntries(params) as Record<string, string>,
+    );
 
     this.value = `${url}?${query.toString()}`;
-  }
-
-  private getNonEmptyFilters(filters: FilterType) {
-    if (filters === undefined) return {};
-
-    return Object.fromEntries(
-      Object.entries(filters).filter(
-        ([_key, value]) => value !== undefined && value !== null,
-      ),
-    ) as Record<string, string>;
   }
 }
